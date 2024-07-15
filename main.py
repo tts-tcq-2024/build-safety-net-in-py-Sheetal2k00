f2b0fed00691f300b8e3b268b906e2dc62f68af7
Sheetal2k00 <pavaskarsheetal@gmail.com>
2024-07-15 16:32:45 +0530

    Create assignment

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-
 def soundex(name):
+    # Soundex mappings
     soundex_mapping = {
         'b': '1', 'f': '1', 'p': '1', 'v': '1',
         'c': '2', 'g': '2', 'j': '2', 'k': '2', 'q': '2', 's': '2', 'x': '2', 'z': '2',
@@ -14,10 +13,10 @@ def soundex(name):
     first_letter = name[0].upper()
     encoded_name = first_letter
     
-    name = re.sub(r'[aeiouyhw]', '', name[1:])
-    
     previous_digit = ''
-    for char in name:
+    for char in name[1:]:
+        if char in 'aeiouyhw':
+            continue
         digit = soundex_mapping.get(char, '0')
         if digit != previous_digit:
             encoded_name += digit
@@ -25,3 +24,10 @@ def soundex(name):
     
     encoded_name = encoded_name.replace('0', '')
     return (encoded_name + '000')[:4]
+
+# Testing Soundex Implementation
+print(soundex("User"))    # Expected: U260
+print(soundex("Clam"))    # Expected: C450
+print(soundex("India"))   # Expected: I530
+print(soundex("Kumta"))   #Expected: K530
+
